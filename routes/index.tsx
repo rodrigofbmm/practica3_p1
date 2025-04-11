8d0fd53f2002a9cf9ffaa4d450b95d4b0b2b08d7
rodrigofbmm <115274224+rodrigofbmm@users.noreply.github.com>
2025-04-11 16:41:22 +0200

    Update index.tsx

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,4 +1,3 @@
-import Axios from 'npm:axios@1.3.4';
 import { FreshContext, Handlers, PageProps } from "$fresh/server.ts";
 import BookComponent from "../components/BookComponent.tsx";
 
@@ -42,38 +41,37 @@ const featuredBooks = [
 
 export const handler: Handlers = {
   GET: async (_req: Request, ctx: FreshContext<unknown, Data>) => {
-    
-      const results: Book[] = [];
+    const books: Book[] = [];
 
+    for (const title of featuredBooks) {
+      const url = `https://openlibrary.org/search.json?q=${encodeURIComponent(title)}`;
+      const res = await fetch(url);
+      const json: bookAPI = await res.json();
+      const data = json.docs[0];
 
-      const books = await Promise.all(
-        featuredBooks.map(async (title) => {
-            const url = `https://openlibrary.org/search.json?q=${encodeURIComponent(title)}`;
-            const response = await Axios.get<bookAPI>(url);
-            const data = response.data.docs[0]; 
-          return {
-            id: data.key.replace("/works/", ""),
-            title: data.title,
-            cover: data.cover_i
-          ? `https://covers.openlibrary.org/b/id/${data.cover_i}-L.jpg`
-          : "No hay portada disponible",
-            author: data.author_name?.[0] ?? "Autor desconocido"
-          };
-        })
-      );
-      return ctx.render({ books });
-    },
+      if (data) {
+        books.push({
+          id: data.key.replace("/works/", ""),
+          title: data.title,
+          cover: data.cover_i
+            ? `https://covers.openlibrary.org/b/id/${data.cover_i}-L.jpg`
+            : "No hay portada disponible",
+          author: data.author_name?.[0] ?? "Autor desconocido",
+        });
+      }
+    }
 
-  
+    return ctx.render({ books });
+  },
 };
 
 const Page = (props: PageProps<Data>) => {
   return (
     <div>
-        <a href = "/search">Buscar un libro</a>
-        <BookComponent books={props.data.books} />;
+      <a href="/search">Buscar un libro</a>
+      <BookComponent books={props.data.books} />
     </div>
-  )
+  );
 };
 
 export default Page;
