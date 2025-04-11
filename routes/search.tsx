464a9ecec38c94d71e6a126ab9fa67d25998dcb4
rodrigofbmm <115274224+rodrigofbmm@users.noreply.github.com>
2025-04-11 16:40:58 +0200

    Update search.tsx

--- routes/search.tsx
+++ routes/search.tsx
@@ -1,7 +1,5 @@
-import Axios from 'npm:axios@1.3.4';
 import { FreshContext, Handlers, PageProps } from "$fresh/server.ts";
 import SearchComponent from "../components/SearchComponent.tsx";
-
 
 type Book = {
   id: string;
@@ -31,32 +29,30 @@ export const handler: Handlers = {
     if (!title) return ctx.render({ books: [] });
 
     const url = `https://openlibrary.org/search.json?q=${encodeURIComponent(title)}`;
+    const res = await fetch(url);
+    const data: bookAPI = await res.json();
 
+    const books: Book[] = data.docs.map((doc) => ({
+      id: doc.key.replace("/works/", ""),
+      title: doc.title,
+      cover: doc.cover_i
+        ? `https://covers.openlibrary.org/b/id/${doc.cover_i}-L.jpg`
+        : "No hay portada disponible",
+      author: doc.author_name?.[0] ?? "Autor desconocido",
+    }));
 
-    const response = await Axios.get<bookAPI>(url);
-    const books: Book[] = response.data.docs.map((doc) => ({
-        id: doc.key.replace("/works/", ""),
-        title: doc.title,
-        cover: doc.cover_i
-          ? `https://covers.openlibrary.org/b/id/${doc.cover_i}-L.jpg`
-          : "No hay portada disponible",
-        author: doc.author_name?.[0] ?? "Autor desconocido",
-        }));
-
-        return ctx.render({ books });
-
-    },
+    return ctx.render({ books });
+  },
 };
 
 const Page = (props: PageProps<Data>) => {
-    const { books } = props.data;
-  
-    return (
-      <div>
-        <SearchComponent books={books} />
-      </div>
-    );
-  };
-  
+  const { books } = props.data;
+
+  return (
+    <div>
+      <SearchComponent books={books} />
+    </div>
+  );
+};
 
 export default Page;
